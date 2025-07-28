5b84167857bb3b2d11520e3f81f60af3d1058d00
daramsreya <daramsreya83@gmail.com>
2025-07-28 22:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function volume_sphere() {
+function volume_sphere(event) {
+	event.preventDefault();
     //Write your code here
-  
+	const r=parseFloat(document.getElementById(".radius").value);
+	const v=document.getElementById("volume");
+	if (isNaN(r) || r < 0) {
+        alert("Please enter a valid non-negative number for radius.");
+        volumeInput.value = "";
+        return;
+    }
+	const vol= (4/3)* Math.PI *Math.pow(r,3);
+	v.value=vol.toFixed(4);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload =function(){ document.getElementById('MyForm').onsubmit = volume_sphere; };
